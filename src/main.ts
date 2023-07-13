d55e7c74afdccae0ee08d28ad262d6bb9671f84f
Roland <machodin@yandex.ru>
2023-07-13 19:13:55 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORt || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('Бекенд на nestjs')
     .setDescription('Документация REST API')
