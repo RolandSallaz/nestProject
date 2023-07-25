b3442c00b7c6d622bfe24244050955088899ed72
Roland <machodin@yandex.ru>
2023-07-25 18:55:47 +0300

    added vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,19 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      "https://rolandsallaz.github.io/ReactChat",
-      "http://localhost:5173"
-    ],
-    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"],
-    credentials: true
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
-    .setTitle("Бекенд на nestjs")
-    .setDescription("Документация REST API")
-    .setVersion("0.1")
+    .setTitle('Бекенд на nestjs')
+    .setDescription('Документация REST API')
+    .setVersion('0.1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("/api/docs", app, document);
+  SwaggerModule.setup('/api/docs', app, document);
 
-  await app.listen(PORT, () => console.log("Server started"));
+  await app.listen(PORT, () => console.log('Server started'));
 }
 
 bootstrap();
